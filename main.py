7c6efe2185140a4bc0a1ba877eb2cff3e4deb934
noomesk <aplazasn@unal.edu.co>
2025-12-21 22:58:01 -0500

    Clase variables

---
+++ main.py
@@ -0,0 +1,9 @@
+#Este es un comentario de una sola línea
+
+print("Hola mundo desde un script en python")
+"""Este es un comentario de varias líneas,
+cuando uso comillas triples, puedo usar
+multiples líneas sin tener que usar \n jeje 
+¿qué cool no?
+ """
+ 
---
+++ variables.py
@@ -0,0 +1,61 @@
+"""para crear una variable en python se usa el signo igual.
+Por ejemplo: <variable> = <valor>
+""" 
+# ¿En python es necesario que definamos el tipo de variable?
+#no, ya que python es un lenguaje de programación de tipado dinámico
+#Eso quiere decir q nuestras variables van a inferir su tipo de acuerdo al valor que le asignemos. 
+
+name = "Angie"
+print(name)
+
+#para ejecutar el codigo: ctrl+f5 sin depuración y f5 con dep
+
+
+#para conocer el tipo de la variable
+type(name)
+#para imprimir el valor en consola
+print(type(name))
+
+#ahora con otro tipo de variable
+age = 26
+print(age)
+type(age)
+print(type(age))
+
+#esta es una variable de tipo entero "int"
+
+# n python no importa si usamos mayúsculas en las variables, por ejemplo:
+FirstName = "Pablo"
+print(FirstName)
+
+#también podemos usar variables de un solo caracter, z.b:
+
+x = "sócrates"
+print(x)
+
+""" Sin embargo por buenas prácticas en python lo mejor siempre se´ra nombrar las variables
+según la nomenclatura snake_case que es: <snake_case> = <valor> en este caso la forma correcta
+para la variable FtirstName seria: first_name, recoerdemos que las nomenclaturas no 
+son para los lenguaes, son para otros seres humanos, cuando escribímos código debemos
+hacerlo pensando que el c´ódigo dbe ser legible, entonces siempre tratempos de usar
+la nomenclatura snake_case para nombrar nuestras variables jej"""
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
