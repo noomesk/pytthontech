4186d2909fc1b662755e49f538279adb13948e94
noomesk <aplazasn@unal.edu.co>
2025-12-22 00:17:52 -0500

    Clase de operadores relacionales

---
+++ 7_operadores_relacionales.py
@@ -0,0 +1,28 @@
+#estos operadores nos permiten hacer comparaciones entre valores
+#por ejemplo comparar ya sean num int o float
+#son: ==, !=, >, <, >=, <=
+# != (significa distinto o diferente)
+# == (significa igual)
+# > (significa mayor que)
+# < (significa menor que)
+# >= (significa mayor o igual que)
+# <= (significa menor o igual que)
+ 
+#Siempre que usemos estos operadores obbtendremos un booleano.
+
+number_one = 10 
+number_zwei = 20 
+
+#result = number_one == number_zwei
+#print (result)
+#como 10 no es igual a 20, obtenemos False
+#print(type(result))
+
+#ahora probemos con otro operador:
+result = number_one <= number_zwei
+print (result)
+print(type(result))
+
+#muy bien y así sucesivamente con cada op
+#también puedo hacerlo con valores flotantes
+#como 10.0 y 2.0 por ejem
