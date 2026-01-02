335eae2d17aaf6281c5e25727316d8dc618313a5
noomesk <aplazasn@unal.edu.co>
2026-01-01 23:50:19 -0500

    Update 1_main.py

--- 1_main.py
+++ 1_main.py
@@ -4,6 +4,8 @@ print("Hola mundo desde un script en python")
 """Este es un comentario de varias líneas,
 cuando uso comillas triples, puedo usar
 multiples líneas sin tener que usar \n jeje 
-¿qué cool no?
+¿qué cool no? bueno es muy importante aprenderlo a 
+usar cuando necesitemos comentarios largos, aunque
+los comentarios largos hacen que el codigo se vea sucio.
  """
  
